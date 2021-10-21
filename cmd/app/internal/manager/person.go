cc246753fe48f1992eb6bfac340049805a227354
Stanislav Kustov <stanislav.kustov@iqoption.com>
2021-10-22 01:51:53 +0300

    Added person loading

---
+++ cmd/app/internal/manager/person.go
@@ -0,0 +1,136 @@
+package manager
+
+import (
+	"compress/gzip"
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	gzViper "github.com/gozix/viper/v2"
+	gzzap "github.com/gozix/zap/v2"
+	"github.com/sarulabs/di/v2"
+	"gitlab.backend.keenetic.link/imdb-graph/app/cmd/app/internal/database"
+	"gitlab.backend.keenetic.link/imdb-graph/app/cmd/app/internal/domain"
+	"gitlab.backend.keenetic.link/imdb-graph/app/gozix/pwd"
+	"go.uber.org/zap"
+)
+
+const nameFilenameConfigPath = "files.names"
+
+type nameManager struct {
+	logger   *zap.Logger
+	repo     domain.PersonRepository
+	filepath string
+}
+
+//compile time check.
+var _ domain.Loader = (*nameManager)(nil)
+
+//DefPersonManagerName is definition name.
+const DefPersonManagerName = "personManager"
+
+//DefPersonManager is definition getter.
+func DefPersonManager() di.Def {
+	return di.Def{
+		Name: DefPersonManagerName,
+		Tags: []di.Tag{{
+			Name: domain.DefTagLoader,
+		}},
+		Build: func(ctn di.Container) (_ interface{}, err error) {
+			var (
+				logger  = ctn.Get(gzzap.BundleName).(*zap.Logger).Named(DefPersonManagerName)
+				config  = ctn.Get(gzViper.BundleName).(*gzViper.Viper).Sub("app.imdb")
+				dirData = ctn.Get(pwd.BundleName).(*pwd.PWD)
+				repo    = ctn.Get(database.DefNamesRepositoryName).(domain.PersonRepository)
+			)
+
+			return &nameManager{
+				logger:   logger,
+				repo:     repo,
+				filepath: fmt.Sprintf("%s/%s/%s", dirData.CurrentDir(), config.GetString("download_path"), config.GetString(nameFilenameConfigPath)),
+			}, nil
+		},
+	}
+}
+
+func (m *nameManager) Save() error {
+
+	return nil
+}
+
+func (m *nameManager) Load(ctx context.Context) error {
+
+	f, err := os.Open(m.filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	gr, err := gzip.NewReader(f)
+	if err != nil {
+		return err
+	}
+	defer gr.Close()
+
+	cr := csv.NewReader(gr)
+	cr.Comma = '\t'
+	var i int64
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		i++
+		if i == 1 {
+			continue
+		}
+
+		rec, err := cr.Read()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return err
+		}
+
+		var birthY, deathY int64
+		birthY, _ = strconv.ParseInt(rec[2], 10, 8)
+		deathY, _ = strconv.ParseInt(rec[3], 10, 8)
+
+		var profs = make([]string, 0)
+		if rec[4] != "" {
+			profs = strings.Split(rec[4], ",")
+		}
+		var kft = make([]domain.TitleID, 0)
+		if rec[5] != "" {
+			for _, str := range strings.Split(rec[5], ",") {
+				kft = append(kft, domain.TitleID(str))
+			}
+		}
+
+		item := domain.Person{
+			ID:                 domain.PersonID(rec[0]),
+			PrimaryName:        rec[1],
+			BirthYear:          int16(birthY),
+			DeathYear:          int16(deathY),
+			PrimaryProfessions: profs,
+			KnownForTitles:     kft,
+		}
+		if err = m.repo.Save(item); err != nil {
+			return err
+		}
+
+		if i%100 == 0 {
+			m.logger.Info("Processed lines", zap.Int64("lines", i))
+		}
+	}
+}
+
+func (m *nameManager) Name() string {
+	return DefPersonManagerName
+}
