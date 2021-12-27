e89fb84adecff1898666f7a0d40a7476cf543773
Stanislav Kustov <stanislav.kustov@iqoption.com>
2021-12-27 16:15:33 +0300

    Refactor and beginning of relations

--- cmd/app/internal/manager/movie.go
+++ cmd/app/internal/manager/movie.go
@@ -1,14 +1,82 @@
 package manager
 
 import (
-	neo "github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"context"
+	"fmt"
+	"github.com/spf13/viper"
+
+	gzViper "github.com/gozix/viper/v2"
+	gzzap "github.com/gozix/zap/v2"
+	"github.com/sarulabs/di/v2"
+	"gitlab.backend.keenetic.link/imdb-graph/app/cmd/app/internal/database"
+	"gitlab.backend.keenetic.link/imdb-graph/app/cmd/app/internal/domain"
+	"gitlab.backend.keenetic.link/imdb-graph/app/gozix/pwd"
+	"go.uber.org/zap"
 )
 
+const movieFilenameConfigPath = "files.movies"
+
 type movieManager struct {
-	neoClient neo.Session
+	logger   *zap.Logger
+	repo     domain.MovieRepository
+	filepath string
+	config   *viper.Viper
 }
 
-func (m *movieManager) Save() error {
+//compile time check.
+var _ domain.LoaderRelations = (*movieManager)(nil)
 
+//DefMovieManagerName is definition name.
+const DefMovieManagerName = "movieManager"
+
+//DefMovieManager is definition getter.
+func DefMovieManager() di.Def {
+	return di.Def{
+		Name: DefMovieManagerName,
+		Tags: []di.Tag{{
+			Name: domain.DefTagLoaderRelations,
+		}, {
+			Name: domain.DefTagLoaderEntities,
+		}},
+		Build: func(ctn di.Container) (_ interface{}, err error) {
+			var (
+				logger  = ctn.Get(gzzap.BundleName).(*zap.Logger).Named(DefMovieManagerName)
+				config  = ctn.Get(gzViper.BundleName).(*gzViper.Viper).Sub("app.imdb")
+				dirData = ctn.Get(pwd.BundleName).(*pwd.PWD)
+				repo    = ctn.Get(database.DefMoviesRepositoryName).(domain.MovieRepository)
+			)
+
+			return &movieManager{
+				logger:   logger,
+				repo:     repo,
+				filepath: fmt.Sprintf("%s/%s/%s", dirData.CurrentDir(), config.GetString("download_path"), config.GetString(movieFilenameConfigPath)),
+				config:   config,
+			}, nil
+		},
+	}
+}
+
+func (m *movieManager) LoadRelations(ctx context.Context) error {
 	return nil
+}
+
+func (m *movieManager) Name() string {
+	return DefMovieManagerName
+}
+
+func (m *movieManager) LoadEntities(ctx context.Context) (err error) {
+	var complete = make(chan struct{})
+	go func() {
+		err = m.repo.LoadFromCSV(m.config.GetString(movieFilenameConfigPath))
+		complete <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-complete:
+			return
+		}
+	}
 }
--- cmd/app/internal/manager/person.go
+++ cmd/app/internal/manager/person.go
@@ -1,15 +1,8 @@
 package manager
 
 import (
-	"compress/gzip"
 	"context"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-
 	"github.com/spf13/viper"
 
 	gzViper "github.com/gozix/viper/v2"
@@ -21,7 +14,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const nameFilenameConfigPath = "files.names"
+const personFilenameConfigPath = "files.persons"
 
 type nameManager struct {
 	logger   *zap.Logger
@@ -50,13 +43,13 @@ func DefPersonManager() di.Def {
 				logger  = ctn.Get(gzzap.BundleName).(*zap.Logger).Named(DefPersonManagerName)
 				config  = ctn.Get(gzViper.BundleName).(*gzViper.Viper).Sub("app.imdb")
 				dirData = ctn.Get(pwd.BundleName).(*pwd.PWD)
-				repo    = ctn.Get(database.DefNamesRepositoryName).(domain.PersonRepository)
+				repo    = ctn.Get(database.DefPersonsRepositoryName).(domain.PersonRepository)
 			)
 
 			return &nameManager{
 				logger:   logger,
 				repo:     repo,
-				filepath: fmt.Sprintf("%s/%s/%s", dirData.CurrentDir(), config.GetString("download_path"), config.GetString(nameFilenameConfigPath)),
+				filepath: fmt.Sprintf("%s/%s/%s", dirData.CurrentDir(), config.GetString("download_path"), config.GetString(personFilenameConfigPath)),
 				config:   config,
 			}, nil
 		},
@@ -68,72 +61,19 @@ func (m *nameManager) Save() error {
 	return nil
 }
 
-func (m *nameManager) LoadRelations(ctx context.Context) error {
-	return nil
+func (m *nameManager) LoadRelations(ctx context.Context) (err error) {
+	var complete = make(chan struct{})
+	go func() {
+		err = m.repo.LoadRelationsFromCSV(m.config.GetString(personFilenameConfigPath))
+		complete <- struct{}{}
+	}()
 
-	f, err := os.Open(m.filepath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	gr, err := gzip.NewReader(f)
-	if err != nil {
-		return err
-	}
-	defer gr.Close()
-
-	cr := csv.NewReader(gr)
-	cr.Comma = '\t'
-	var i int64
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
-		default:
-		}
-
-		rec, err := cr.Read()
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-			}
-			return err
-		}
-
-		i++
-		if i == 1 {
-			continue
-		}
-
-		var birthY, deathY int64
-		birthY, _ = strconv.ParseInt(rec[2], 10, 8)
-		deathY, _ = strconv.ParseInt(rec[3], 10, 8)
-
-		var profs = make([]string, 0)
-		if rec[4] != "" {
-			profs = strings.Split(rec[4], ",")
-		}
-		var kft = make([]domain.TitleID, 0)
-		if rec[5] != "" {
-			for _, str := range strings.Split(rec[5], ",") {
-				kft = append(kft, domain.TitleID(str))
-			}
-		}
-
-		item := domain.Person{
-			ID:                 domain.PersonID(rec[0]),
-			PrimaryName:        rec[1],
-			BirthYear:          int16(birthY),
-			DeathYear:          int16(deathY),
-			PrimaryProfessions: profs,
-			KnownForTitles:     kft,
-		}
-		if err = m.repo.Save(item); err != nil {
-			return err
-		}
-
-		if i%10 == 0 {
-			m.logger.Info("Processed lines", zap.Int64("lines", i))
+		case <-complete:
+			return
 		}
 	}
 }
@@ -145,7 +85,7 @@ func (m *nameManager) Name() string {
 func (m *nameManager) LoadEntities(ctx context.Context) (err error) {
 	var complete = make(chan struct{})
 	go func() {
-		err = m.repo.LoadFromCSV(m.config.GetString(nameFilenameConfigPath))
+		err = m.repo.LoadFromCSV(m.config.GetString(personFilenameConfigPath))
 		complete <- struct{}{}
 	}()
 
