2110ee151063d16b7f5d6c3ff0f7b805ed7ea225
Simon <spam@esemi.ru>
2022-01-07 16:23:24 +0100

    Display mocked graph (#25)
    * :wip: init sigmajs graph
    
    * :wip:
    
    * :recycle: Rm sigma.js
    
    * :sparkles: Use ogma

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,22 +1,21 @@
 "use strict";
 
 const searchApiEndpoint = './mock-api/search.json';
-const neighboursApiEndpoint = './mock-api/neighbours.json';
+const nodeInfoApiEndpoint = './mock-api/node-info.json';
 
 window.addEventListener('load', function () {
     console.log('app started');
 
-    //graph init
-    let graph = new GraphEngine();
+    // init graph app
+    let graphApp = new GraphEngine(nodeInfoApiEndpoint);
 
-    // search input
-    init_search_app(graph);
-
+    // init search app
+    init_search_app(graphApp, searchApiEndpoint);
 
 }, false);
 
 
-function init_search_app(graph) {
+function init_search_app(graph, searchApi) {
     const minSearchQueryLength = 3;
     const nodeIdAttr = 'data-id';
     const searchInput = document.getElementById('search-form-input');
@@ -28,11 +27,10 @@ function init_search_app(graph) {
 
     let search_handler = (e) => {
         let searchString = e.target.value.trim().toLowerCase();
-        console.log(searchString);
 
         if (!!searchString) {
             searchResultContainer.hidden = true;
-            graph.displayNotInitState();
+            graph.showPreInitState();
         }
 
         if (searchString.length < minSearchQueryLength) {
@@ -45,7 +43,7 @@ function init_search_app(graph) {
         }
 
         debounce = setTimeout(() => {
-            let url = searchApiEndpoint + '?' + new URLSearchParams({search: searchString,});
+            let url = searchApi + '?' + new URLSearchParams({search: searchString,});
             console.log('send search request', searchString, url);
 
             //abort existing request
@@ -70,11 +68,10 @@ function init_search_app(graph) {
                 })
                 .then((data) => {
                     console.log('fetch search result', data)
-                    //clear search result
 
-                    if (!data.results.length) {
+                    if (!data.hasOwnProperty('results') || !data.results.length) {
                         searchResultContainer.hidden = true;
-                        graph.displayNotFoundState(searchString);
+                        graph.showNotFoundState(searchString);
                         return
                     }
 
@@ -87,6 +84,7 @@ function init_search_app(graph) {
                         createdNode.appendChild(document.createTextNode(node.name));
                         searchResultContainer.appendChild(createdNode);
                     });
+
                     searchResultContainer.hidden = false;
                 });
 
@@ -98,16 +96,15 @@ function init_search_app(graph) {
         searchResultContainer.hidden = true;
         searchInput.value = '';
         searchInput.focus();
-        graph.displayNotInitState();
+        graph.init();
     }
 
 
     let select_handler = (e) => {
         console.log('select root node handler', e.target);
-        //todo lock search-input for new chars
         searchResultContainer.hidden = true;
         searchInput.value = e.target.innerText;
-        graph.init(e.target.getAttribute(nodeIdAttr));
+        graph.addRootNode(e.target.getAttribute(nodeIdAttr));
     }
 
     searchInput.addEventListener("keyup", search_handler, false);
@@ -117,34 +114,167 @@ function init_search_app(graph) {
 
 
 class GraphEngine {
-    root_id;
-    #container;
+    rootNodeId;
+    graph;
+    nodeInfoApiEndpoint;
+    activeRequestController;
 
-    constructor() {
-        this.#container = document.getElementById('graph-container');
-        this.displayNotInitState();
+    static graphContainerId = 'graph-container';
+    static stateNotInitId = 'graph-state-not-init';
+    static stateNotFoundId = 'graph-state-not-found';
+
+    #containerStateNotInit;
+    #containerStateNotFound;
+    #containerForGraph;
+
+    constructor(nodeInfoApiEndpoint) {
+        this.nodeInfoApiEndpoint = nodeInfoApiEndpoint;
+
+        this.#containerStateNotInit = document.getElementById(GraphEngine.stateNotInitId);
+        this.#containerStateNotFound = document.getElementById(GraphEngine.stateNotFoundId);
+        this.#containerForGraph = document.getElementById(GraphEngine.graphContainerId);
+
+        this.init();
     }
 
-    init(root_node_id) {
-        this.root_id = root_node_id;
-        console.log('init graph engine for', root_node_id)
-        // todo init graph
-        // todo init graph-controls
-        let relations = this.fetchNodeRelations(root_node_id);
-        // todo fill graph
+    addRootNode(root_node_id) {
+        // load and display root node with relations
+        console.log('init graph engine for ', root_node_id)
+        this.rootNodeId = root_node_id;
+
+        this._fetchNodeRelations(this.rootNodeId).then((nodeInfo) => {
+            this.appendRelations(nodeInfo.nodes, nodeInfo.edges);
+            this.showGraph();
+        });
     }
 
-    fetchNodeRelations() {
-        neighboursApiEndpoint;
-        return [];
+    _fetchNodeRelations(nodeId) {
+        // fetch all relations by nodeId
+        let url = this.nodeInfoApiEndpoint + '?' + new URLSearchParams({node_id: nodeId,});
+        console.log('send node info request', nodeId, url);
 
+        let nodeInfoRequest = new Request(url, {
+            headers: {'Accept': 'application/json'},
+            method: 'GET',
+            signal: this.activeRequestController.signal
+        });
+
+        return fetch(nodeInfoRequest)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Request error, status = " + response.status);
+                }
+                return response.json()
+            })
+            .then((responseJson) => {
+                console.log('fetch node info response', responseJson)
+                return {
+                    nodes: responseJson.nodes,
+                    edges: responseJson.relations,
+                }
+            });
     }
 
-    displayNotInitState() {
-        this.#container.innerText = 'Use search to explore a couscous';
+    appendRelations(nodes, edges) {
+        // append nodes and edges to graph
+        this.graph.addNodes(
+            nodes.map(node => {
+                return {
+                    id: node.id,
+                    attributes: {
+                        text: node.name,
+                    },
+                    data: {
+                        name: node.name,
+                        type: node.type,
+                    }
+                }
+            })
+        );
+        this.graph.addEdges(
+            edges.map(edge => {
+                return {
+                    id: `${edge.from_id}:${edge.to_id}`,
+                    source: edge.from_id,
+                    target: edge.to_id,
+                    attributes: {
+                        text: `${edge.type}[${edge.name}]`
+                    },
+                    data: {
+                        name: edge.name,
+                        type: edge.type,
+                    }
+                }
+            })
+        );
+
+        console.log('debug: edges=', this.graph.getNodes().size, '; nodes=', this.graph.getEdges().size)
     }
 
-    displayNotFoundState(searchString) {
-        this.#container.innerText = 'No results for ' + searchString;
+    init() {
+        if (!!this.activeRequestController) {
+            this.activeRequestController.abort();
+        }
+        this.activeRequestController = new AbortController();
+
+        // @see <https://doc.linkurio.us/ogma/3.2.0/quickstart.html>
+        this.graph = new Ogma({
+            container: this.#containerForGraph.id,
+            options: {
+                directedEdges: true,
+                edgesAlwaysCurvy: true,
+            }
+        });
+        this.graph.styles.addNodeRule({
+            color: this.graph.rules.map({
+                field: 'type',
+                values: {
+                    'movie': 'green',
+                    'name': 'yellow',
+                },
+                fallback: 'black'
+            })
+        });
+
+        //
+        this.graph.events.onClick(event => {
+            const target = event.target;
+            if (!target || !target.isNode) {
+                return
+            }
+            console.log(`Node ${target.getId()} was clicked`);
+        });
+
+        this.showPreInitState();
+    }
+
+    drawGraph() {
+        // apply graph layout and draw results
+        this.graph.layouts.force({
+            incremental: true,
+        })
+    }
+
+    showGraph() {
+        this.hideAll();
+        this.#containerForGraph.hidden = false;
+        this.drawGraph();
+    }
+
+    showPreInitState() {
+        this.hideAll();
+        this.#containerStateNotInit.hidden = false
+    }
+
+    showNotFoundState(searchString) {
+        this.hideAll();
+        this.#containerStateNotFound.innerText = 'No results for ' + searchString;
+        this.#containerStateNotFound.hidden = false
+    }
+
+    hideAll() {
+        this.#containerStateNotInit.hidden = true;
+        this.#containerStateNotFound.hidden = true;
+        this.#containerForGraph.hidden = true;
     }
 }
